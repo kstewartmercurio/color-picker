ab8c84386b10f3da405976e953c99f57906583dd
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-10-09 01:38:11 -0400

    issue with buttons not displaying the correct color

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
@@ -9,19 +9,29 @@ function Game() {
     const [btn2, setBtn2] = useState(color2);
     const color3 = ("#" + Math.floor(Math.random() * 16777215).toString(16))    
     const [btn3, setBtn3] = useState(color3);
+    
+    const [count, setCount] = useState(0);
 
     const backgroundVal = Math.floor(Math.random() * 3);
-        switch (backgroundVal) {
-            case 0:
-                document.body.style.backgroundColor = color1;
-                break;
-            case 1:
-                document.body.style.backgroundColor = color2;
-                break;
-            case 2:
-                document.body.style.backgrounColor = color3;
-                break;
-        };
+    let answer;
+    switch (backgroundVal) {
+        case 0:
+            document.body.style.backgroundColor = color1;
+            answer = 0;
+            break;
+        case 1:
+            document.body.style.backgroundColor = color2;
+            answer = 1;
+            break;
+        case 2:
+            document.body.style.backgrounColor = color3;
+            answer = 2;
+            break;
+        default:
+            break;
+    };
+    const [ans, setAns] = useState(answer);
+    console.log(ans);
 
 
     const changeButtons = () => {
@@ -36,27 +46,50 @@ function Game() {
         switch (backgroundVal) {
             case 0:
                 document.body.style.backgroundColor = color1;
+                setAns(0);
                 break;
             case 1:
                 document.body.style.backgroundColor = color2;
+                setAns(1);
                 break;
             case 2:
                 document.body.style.backgrounColor = color3;
+                setAns(2);
+                break;
+            default:
                 break;
         };
     }
 
     return (
-        <div className="btnRow">
-            <button className="btn" onClick={changeButtons}>
-                {btn1}
-            </button>
-            <button className="btn" onClick={changeButtons}>
-                {btn2}
-            </button>
-            <button className="btn" onClick={changeButtons}>
-                {btn3}
-            </button>
+        <div>
+            <div className="btnRow">
+                <button className="btn" onClick={() => {
+                    if (ans === 0) {
+                        setCount((prev) => prev + 1);
+                    }
+                    changeButtons();
+                }}>
+                    {btn1}
+                </button>
+                <button className="btn" onClick={() => {
+                    if (ans === 1) {
+                        setCount((prev) => prev + 1);
+                    }
+                    changeButtons();
+                }}>
+                    {btn2}
+                </button>
+                <button className="btn" onClick={() => {
+                    if (ans === 2) {
+                        setCount((prev) => prev + 1);
+                    }
+                    changeButtons();
+                }}>
+                    {btn3}
+                </button>
+            </div>
+            <p className="count">correct guesses: {count}</p>
         </div>
     );
 }
